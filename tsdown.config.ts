7b709aaf5523501e8f89c97d033252e6cec3fd7b
pawover <pawover@outlook.com>
2025-12-01 15:51:23 +0800

    init

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: {
-    "index": "src/utils/index.ts",
-    "enums": "src/enums/index.ts",
-    "vite": "src/vite/index.ts",
-    "zod": "src/zod/index.ts",
-    "hooks-alova": "src/hooks/alova/index.ts",
-    "hooks-react": "src/hooks/react/index.ts",
+export default defineConfig([
+  {
+    entry: {
+      index: "src/utils/index.ts",
+      enums: "src/enums/index.ts",
+      vite: "src/vite/index.ts",
+      zod: "src/zod/index.ts",
+    },
+    dts: true,
+    target: "es2020",
+    platform: "neutral",
+    tsconfig: "tsconfig.build.json",
   },
-  dts: true,
-  target: "es2020",
-  platform: "neutral",
-  tsconfig: "tsconfig.build.json",
-});
+  {
+    entry: {
+      "hooks-alova": "src/hooks/alova/index.ts",
+      "hooks-react": "src/hooks/react/index.ts",
+    },
+    dts: true,
+    target: "es2020",
+    platform: "neutral",
+    tsconfig: "tsconfig.build.json",
+  },
+]);
