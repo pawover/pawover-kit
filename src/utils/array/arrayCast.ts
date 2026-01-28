1da4c340a5e936d2ed93d7dfe099dd28e693dc63
pawover <pawover@outlook.com>
2026-01-28 17:44:13 +0800

    init

--- src/utils/array/arrayCast.ts
+++ src/utils/array/arrayCast.ts
@@ -5,10 +5,12 @@ import { isArray, isNull, isUndefined } from "../typeof";
  * @param candidate 待构造项
  * @param checkEmpty 是否检查 `undefined` 和 `null`
  */
-export function arrayCast<T>(candidate: T | T[], checkEmpty = true): T[] {
+export function arrayCast<T>(candidate: T | T[] | null | undefined, checkEmpty?: true): NonNullable<T>[];
+export function arrayCast<T>(candidate: T | T[] | null | undefined, checkEmpty?: false): T[];
+export function arrayCast<T>(candidate: T | T[] | null | undefined, checkEmpty = true): T[] {
   if (checkEmpty && (isUndefined(candidate) || isNull(candidate))) {
     return [];
   }
 
-  return isArray(candidate) ? [...candidate] : [candidate];
+  return isArray(candidate) ? [...candidate] : [candidate as T];
 }
