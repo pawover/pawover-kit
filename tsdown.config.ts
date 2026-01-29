1c929a1ffbf992e3b8e910386185cc92da46519f
pawover <pawover@outlook.com>
2026-01-29 15:45:02 +0800

    init

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,35 +1,21 @@
-import { defineConfig, type UserConfig } from "tsdown";
-
-const defaultUserConfig: UserConfig = {
-  dts: {
-    resolve: ["radashi", "type-fest", "@pawover/types", "ts-toolbelt"],
-  },
-  target: "es2020",
-  platform: "neutral",
-  tsconfig: "tsconfig.build.json",
-};
+import { defineConfig } from "tsdown";
 
 export default defineConfig([
   {
     entry: {
-      index: "src/utils/index.ts",
-      enums: "src/enums/index.ts",
-      vite: "src/vite/index.ts",
-      zod: "src/zod/index.ts",
-    },
-    ...defaultUserConfig,
-  },
-  {
-    entry: {
+      "index": "src/utils/index.ts",
+      "enums": "src/enums/index.ts",
+      "vite": "src/vite/index.ts",
+      "zod": "src/zod/index.ts",
       "hooks-alova": "src/hooks/alova/index.ts",
       "hooks-react": "src/hooks/react/index.ts",
-    },
-    ...defaultUserConfig,
-  },
-  {
-    entry: {
       "patches-fetchEventSource": "src/patches/fetchEventSource/index.ts",
     },
-    ...defaultUserConfig,
+    dts: {
+      resolve: ["radashi", "type-fest", "@pawover/types", "ts-toolbelt"],
+    },
+    target: "es2020",
+    platform: "neutral",
+    tsconfig: "tsconfig.build.json",
   },
 ]);
